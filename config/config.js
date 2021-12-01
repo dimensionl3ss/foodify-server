9c1568b4a8d14ddb389d498a380eeee053aadc62
Adarsh <adarshraj0210@gmail.com>
2021-12-01 22:41:24 +0530

    connection created with mysql

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: 'adarsh',
+        database: 'mydb'
+    }
+}
---
+++ services/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+const config = require('../config/config');
+ 
+
+const connection = mysql.createConnection(config.db);
+
+connection.connect((error) => {
+    if(error) throw error;
+    const table = `create table if not exists users (name varchar(50) not null, email varchar(30) primary key, password varchar(30) not null, address varchar(300) not null, pin int not null)`;
+    connection.query(table, (err, result) => {
+        if(err) throw err;
+        console.log('Table Created!');
+    })
+    console.log('connected successfully!!');
+})
+
+module.exports = connection;
